e4d2b49e107c1350e696d511fd9d6074a0e79b76
VladVolM <57007871+VladVolM@users.noreply.github.com>
2020-01-14 04:33:57 -0800

    Last commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siguienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anteriorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Archivos (*.jpg,*.png)|*.jpg;*.png";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 256);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 142);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Imagen";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(82, 113);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(379, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dimensiones:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(82, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(379, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(82, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(379, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(82, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(379, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(82, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(379, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tamaño:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tipo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Direccion:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(221, 218);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(239, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 108);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.TabStop = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivosToolStripMenuItem,
+            this.imagenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(490, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivosToolStripMenuItem
+            // 
+            this.archivosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivosToolStripMenuItem.Name = "archivosToolStripMenuItem";
+            this.archivosToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.archivosToolStripMenuItem.Text = "Archivos";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.cargarToolStripMenuItem.Text = "Cargar";
+            this.cargarToolStripMenuItem.Click += new System.EventHandler(this.cargarToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // imagenToolStripMenuItem
+            // 
+            this.imagenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eliminarToolStripMenuItem,
+            this.siguienteToolStripMenuItem,
+            this.anteriorToolStripMenuItem});
+            this.imagenToolStripMenuItem.Name = "imagenToolStripMenuItem";
+            this.imagenToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.imagenToolStripMenuItem.Text = "Imagen";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // siguienteToolStripMenuItem
+            // 
+            this.siguienteToolStripMenuItem.Name = "siguienteToolStripMenuItem";
+            this.siguienteToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.siguienteToolStripMenuItem.Text = "Siguiente";
+            this.siguienteToolStripMenuItem.Click += new System.EventHandler(this.siguienteToolStripMenuItem_Click);
+            // 
+            // anteriorToolStripMenuItem
+            // 
+            this.anteriorToolStripMenuItem.Name = "anteriorToolStripMenuItem";
+            this.anteriorToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.anteriorToolStripMenuItem.Text = "Anterior";
+            this.anteriorToolStripMenuItem.Click += new System.EventHandler(this.anteriorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 406);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Y - Visor";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siguienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem anteriorToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            KeyPreview = true;
+            comprobarMovimientos();
+        }
+
+        readonly static int maxSize = 50;
+        string[,] DirImagen = new string[maxSize, 5];
+        int numImagenAUsar = 0;
+        bool sig = false, ant = false, res = false, eli = false, ima = false;
+        void sustituir(int pos)
+        {
+            DirImagen[pos, 0] = DirImagen[pos + 1, 0];
+            DirImagen[pos, 1] = DirImagen[pos + 1, 1];
+            DirImagen[pos, 2] = DirImagen[pos + 1, 2];
+            DirImagen[pos, 3] = DirImagen[pos + 1, 3];
+            DirImagen[pos, 4] = DirImagen[pos + 1, 4];
+        }
+
+        void VerDatos(int pos)
+        {
+            textBox1.Text = DirImagen[pos, 0];
+            textBox2.Text = DirImagen[pos, 1];
+            textBox3.Text = DirImagen[pos, 2];
+            textBox4.Text = DirImagen[pos, 3];
+            textBox5.Text = DirImagen[pos, 4];
+
+            Image imagencargada = Image.FromFile(DirImagen[pos, 1]);
+            pictureBox2.Image = imagencargada;
+            this.Text = DirImagen[pos, 0].Replace(DirImagen[pos, 2], " ") + "- Visor";
+        }
+        void eliminar() {
+            if (listBox1.Items.Count > 0)
+            {
+                DialogResult di = MessageBox.Show("Qiere borrar la imagen?", "Borrar Imagen", MessageBoxButtons.YesNo);
+
+                if (di == DialogResult.Yes)
+                {
+                    //Borrar imagen
+                    for (int i = listBox1.SelectedIndex; i < maxSize - 1; i++)
+                    {
+                        sustituir(i);
+                    }
+                    int index = listBox1.SelectedIndex;
+                    listBox1.Items.RemoveAt(index);
+                    int count = listBox1.Items.Count;
+
+                    if (index==0 && count != 0)
+                        listBox1.SelectedIndex = index;
+                    else if (count != 0)
+                        listBox1.SelectedIndex = index - 1;
+                    if (count == 0)
+                        limpiar();
+
+                    numImagenAUsar -= 1;
+
+                }
+            }
+        }
+
+        void cargar() {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string dir = openFileDialog1.FileName;
+                if (comprobarImagen(openFileDialog1.SafeFileName))
+                    MessageBox.Show("Ya esta en la lista tal archivo", "Repetición");
+                else { 
+                    int index = dir.LastIndexOf("\\");
+                    textBox2.Text = dir;
+                    DirImagen[numImagenAUsar, 1] = dir;
+                    DirImagen[numImagenAUsar, 0] = dir.Substring(index + 1, dir.Length - index - 1);
+                    DirImagen[numImagenAUsar, 2] = dir.Substring(dir.Length - 4, 4);
+                    DirImagen[numImagenAUsar, 3] = new System.IO.FileInfo(dir).Length.ToString();
+
+                    Image imagencargada = Image.FromFile(dir);
+
+                    pictureBox2.Image = imagencargada;
+
+                    DirImagen[numImagenAUsar, 4] = imagencargada.Width + " x " + imagencargada.Height;
+
+                    listBox1.Items.Add(DirImagen[numImagenAUsar, 0]);
+
+                    VerDatos(numImagenAUsar);
+
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    numImagenAUsar += 1;
+                }
+            }
+        }
+
+        void limpiar() {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            textBox5.Text = null;
+            pictureBox2.Image = null;
+            comprobarMovimientos();
+            this.Text = "Y - Visor";
+        }
+
+        void salir() {
+            DialogResult di = MessageBox.Show("Qiere salir?", "Salir", MessageBoxButtons.YesNo);
+
+            if (di == DialogResult.Yes)
+                Application.Exit();
+        }
+        void siguiente() {
+            if (sig)
+            if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+                listBox1.SelectedIndex += 1;
+        }
+        void anterior() {
+            if (ant)
+            if (listBox1.SelectedIndex > 0)
+                listBox1.SelectedIndex -= 1;
+        }
+        void comprobarMovimientos() {
+            int count = listBox1.Items.Count;
+            int index = listBox1.SelectedIndex;
+            res = true;
+            eli = true;
+            ima = true;
+            if (count == 0)
+            {
+                sig = false;
+                ant = false;
+                res = false;
+                eli = false;
+                ima = false;
+            }
+            else if (index <= 0)
+            {
+                ant = false;
+                if (count > 1)
+                    sig = true;
+                else
+                    sig = false;
+            }
+            else if (index >= count - 1)
+            {
+                if (count > 1)
+                    ant = true;
+                else
+                    ant = false;
+                sig = false;
+            }
+            else {
+                ant = true;
+                sig = true;
+            }
+
+            siguienteToolStripMenuItem.Enabled = sig;
+            siguienteToolStripMenuItem.Visible = sig;
+
+            anteriorToolStripMenuItem.Enabled = ant;
+            anteriorToolStripMenuItem.Visible = ant;
+
+            resetToolStripMenuItem.Enabled =res;
+            resetToolStripMenuItem.Visible =res;
+
+            eliminarToolStripMenuItem.Enabled = eli;
+            eliminarToolStripMenuItem.Visible = eli;
+
+            imagenToolStripMenuItem.Visible =ima;
+        }
+        bool comprobarImagen(string nom) {
+            for (int i = 0; i < numImagenAUsar; i++) {
+                if (DirImagen[i, 0] == nom)
+                    return true;
+            }
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salir();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Al cambiar el apartado elegido
+            if (listBox1.SelectedIndex != -1)
+            {
+                int select = listBox1.SelectedIndex;
+                VerDatos(select);
+                //cargar 
+                comprobarMovimientos();
+            }
+        }
+
+        void reset() {
+            if (res) {
+                DialogResult di = MessageBox.Show("Limpiar la lista?", "Reset", MessageBoxButtons.YesNo);
+
+                if (di == DialogResult.Yes) {
+                    listBox1.Items.Clear();
+                    numImagenAUsar = 0;
+                    comprobarMovimientos();
+                }
+            }
+        }
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        private void siguienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            siguiente();
+        }
+
+        private void anteriorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            anterior();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar) {
+                case 's': siguiente(); break;
+                case 'w': anterior(); break;
+                case 'e': eliminar(); break;
+                case 'c': cargar(); break;
+                case 'r': reset(); break;
+                case 'q': salir(); break;
+            }
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
